d99e481aae3111238e78314bfb4683d76ce75bff
Shannon Babincsak <codeinpink@users.noreply.github.com>
2018-08-04 20:46:05 -0500

    Add fonts and change score display

--- frontend/src/components/MatchStatus.js
+++ frontend/src/components/MatchStatus.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './MatchStatus.css'
 import { connect } from 'react-redux'
 
 const Component = ({curScore, playerPiece, bannerMsg}) => {
   return (
     <div>
       <h2>{bannerMsg}</h2>
-      <h3>Score x: {curScore.x} o: {curScore.o}</h3>
+      <h2>Score</h2>
+      <div class="scores">
+        <span>X: <strong>{curScore.x}</strong></span>
+        <span>O: <strong>{curScore.o}</strong></span>
+      </div>
     </div>
   )
 }
